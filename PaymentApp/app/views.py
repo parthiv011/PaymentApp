3dd85d029715b7b236a2b524a6e7207b2bf44b17
parthiv <parmarparthiv77@gmail.com>
2023-08-15 23:33:44 +0530

    Configuring the files and adding templates

--- PaymentApp/app/views.py
+++ PaymentApp/app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request,'home.html')
+
+def loginView(request):
+    return render(request,'auth/login.html')
+
+def signupView(request):
+    return render(request,'auth/signup.html')
