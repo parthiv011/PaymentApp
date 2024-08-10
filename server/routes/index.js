61fd4cc70b244870f075967219fd2e93d144faf3
parthiv <parmarparthiv77@gmail.com>
2024-08-10 09:34:36 +0530

    vercel config

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,6 +3,10 @@ const router = express.Router();
 const userRouter = require('./user');
 const accountRouter = require('./account');
 
+router.use('/', (res) => {
+  res.json('Welcome to the API');
+});
+
 router.use('/user', userRouter);
 router.use('/account', accountRouter);
 
